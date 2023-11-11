4f1eb5c10632bd98441449ccc58968746af2c269
tawkeer1 <117712172+tawkeer1@users.noreply.github.com>
2023-11-12 01:59:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+import{
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+export default class App extends Component {
+  pageSize=6
+  apikey = process.env.REACT_APP_NEWS_API_KEY
+  state={
+    progress:0
+  }
+  setProgress=(progress)=>{
+  this.setState({
+    progress: progress
+  })
+  }
+  render() {
+    return (
+      <>
+      <BrowserRouter basename='/'>
+     
+      <NavBar/>
+      <LoadingBar
+        color='#f11946'
+        height={3}
+        progress={this.state.progress}
+      />
+      <Routes>
+      <Route exact path='/' element = { <News setProgress={this.setProgress} apikey={this.apikey} key="general" pageSize ={this.pageSize} country='in' category="general"/>} />
+      <Route exact path='/business' element = { <News setProgress={this.setProgress} apikey={this.apikey} key="business" pageSize ={this.pageSize} country='in' category="business"/>} />
+      <Route exact path='/entertainment' element = { <News setProgress={this.setProgress} apikey={this.apikey} key="entertainment" pageSize ={this.pageSize} country='in' category="entertainment"/>} />
+      <Route exact path='/sports' element = { <News setProgress={this.setProgress} apikey={this.apikey} key="sports" pageSize ={this.pageSize} country='in' category="sports"/>} />
+      <Route exact path='/technology' element = { <News setProgress={this.setProgress} apikey={this.apikey} key="technology" pageSize ={this.pageSize} country='in' category="technology"/>} />
+      <Route exact path='/science' element = { <News setProgress={this.setProgress} apikey={this.apikey} key="science" pageSize ={this.pageSize} country='in' category="science"/>} />
+      </Routes>
+      </BrowserRouter>
+      </>
+    )
+  }
+}
+
